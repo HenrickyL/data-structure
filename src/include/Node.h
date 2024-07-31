8c1035c4b4d07daa8d79c5b21b22f38d604bf636
HenrickyL <HenrickyL1@gmail.com>
2024-07-31 10:41:23 -0300

    adjust Node

--- src/include/Node.h
+++ src/include/Node.h
@@ -4,8 +4,14 @@ namespace EDA{
     template<typename T>
     struct Node{
         T value;
-        Node* left;
-        Node* right;
+        Node<T>* left;
+        Node<T>* right;
+
+        Node(T _value_, Node<T>* _left = nullptr, Node<T>* _right = nullptr){
+            value = _value_;
+            left = _left;
+            right = _right; 
+        }
     };
 }
 #endif
